0921df4bb0b29da10a492b722e7a495cf47aec40
Teguh Pratama <teguh@indovti.com>
2018-12-14 13:43:56 +0700

    fix db seed

--- order/database/seeds/DatabaseSeeder.php
+++ order/database/seeds/DatabaseSeeder.php
@@ -16,7 +16,16 @@ class DatabaseSeeder extends Seeder
 
         \App\Models\Invoice::truncate();
         \App\Models\Cart::truncate();
-        
+
+        factory(\App\Models\Cart::class, 6)->create();
+
+        factory(\App\Models\Cart::class, 1)->create(['status' => 'pending', 'qty' => 2, 'invoice_id' => 1])->each(
+            function($cart) {
+                factory(\App\Models\Invoice::class, 1)->create([
+                    'total' => ($cart->price * $cart->qty)
+                ]);
+            }
+        );
 
         // Enable it back
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
